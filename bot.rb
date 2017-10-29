1177498c0aa879048503f081f32cd0aa48c451b3
wemmm <jenny.wem@gmail.com>
2017-10-29 10:54:27 +0000

    Adds bot.rb

---
+++ bot.rb
@@ -0,0 +1,10 @@
+require 'Twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
+end
+
+client.update("hello world")
